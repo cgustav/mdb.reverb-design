ee9848b8557d17c9ea681045e107eb05c8b0bafb
Edomun <gstv.qnd@outlook.com>
2018-08-21 00:37:07 -0300

    pre v0.1.2 commit2
    Aux, improvements

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     new WOW().init();
     //alert('We\'re ready');
+    // SideNav Button Initialization
+    //$(".button-collapse").sideNav();
+    // SideNav Scrollbar Initialization
+    //var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    //Ps.initialize(sideNavScrollbar);
 })
 
